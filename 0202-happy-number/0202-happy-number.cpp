3474f280986237dcacf53a628a8142be907b04e9
Ishan Purohit <68988267+Ishan-77@users.noreply.github.com>
2022-10-25 18:18:43 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (84.62%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    
+    int findSquare(int num)
+    {
+        int ans =0;
+        while(num>0)
+        {
+            int rem =  num%10;
+            ans += rem*rem;
+            num=num/10;
+        }
+        return ans;
+    }
+    bool isHappy(int n) {
+       int slow = n;
+        int fast = n;
+        do {
+            slow=findSquare(slow);
+            fast=findSquare(findSquare(fast));
+        } while(slow != fast);
+        
+        if(slow==1)
+        {
+            return true;
+        }
+        return false;
+       
+    }
+};
