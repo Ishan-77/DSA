17f67b79bbac25a379ea23e3d67cf827f0050f13
Ishan Purohit <68988267+Ishan-77@users.noreply.github.com>
2023-01-15 22:34:23 +0530

    Time: 20 ms (82.58%), Space: 17 MB (55.32%) - LeetHub

---
+++ 0136-single-number/0136-single-number.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int ans=0;
+        for(auto a:nums)
+        {
+            ans=ans^a;
+        }
+        return ans;
+    }
+};
