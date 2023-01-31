ed1c523d56c418201ce2d2fd68932de397b9362f
Ishan Purohit <68988267+Ishan-77@users.noreply.github.com>
2023-01-31 23:40:33 +0530

    Time: 3 ms (38.90%), Space: 5.9 MB (73.52%) - LeetHub

---
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int tribonacci(int n) {
+        int f[38]={0};
+        f[0]=0;
+        f[1]=1;
+        f[2]=1;
+        int i;
+        for( i=3;i<=n;i++)
+        {
+            f[i] = f[i-1]+f[i-2]+f[i-3];
+        }
+        return f[n];
+    }
+};
