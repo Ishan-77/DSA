ad8f8b390782ae0b649f5e5c29b946d03287e12a
Ishan Purohit <68988267+Ishan-77@users.noreply.github.com>
2023-02-06 22:24:45 +0530

    Time: 3 ms (92.72%), Space: 9.7 MB (61.93%) - LeetHub

---
+++ 1470-shuffle-the-array/1470-shuffle-the-array.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> shuffle(vector<int>& nums, int n) {
+        vector<int>ans;
+        int i=0;
+        int j= nums.size()-n;
+       
+        while(i<j && j<nums.size())
+        {
+            ans.push_back(nums[i]);
+            ans.push_back(nums[j]);
+            i++;
+            j++;
+        }
+        
+        return ans;
+    }
+};
