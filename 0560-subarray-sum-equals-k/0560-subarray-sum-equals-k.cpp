00864b4ed62cc0968c9d2da954c2a75afefb744e
Ishan Purohit <68988267+Ishan-77@users.noreply.github.com>
2023-04-13 00:44:27 +0530

    Time: 99 ms (51.99%), Space: 41.6 MB (60.38%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -4,11 +4,12 @@ public:
         int count = 0;
         int psum=0;
         unordered_map<int,int>mpp;
+         // imp step
         mpp[0]=1;
     
         for(int i=0;i<nums.size();i++)
         {
-            psum+=nums[i];
+            psum+=nums[i]; // prefix sum
             if(mpp.find(psum-k)!=mpp.end())
             {
                 count+=mpp[psum-k];
