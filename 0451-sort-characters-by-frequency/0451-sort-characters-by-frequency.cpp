94a4ab6f69a4678cebc63a489bef3218d9e6645a
Ishan Purohit <68988267+Ishan-77@users.noreply.github.com>
2023-04-21 23:43:34 +0530

    Time: 15 ms (33.43%), Space: 8.4 MB (83.08%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -1,31 +1,33 @@
 class Solution {
 public:
     
-    static bool comp(pair<char,int> a,pair<char,int> b)
+    static bool comp(pair<char,int> a, pair<char,int> b)
     {
-        return a.second>b.second;
+        return a.second>b.second;  
     }
-     
+    
     string frequencySort(string s) {
-      unordered_map<char,int> mp;
-        
-        for(int i=0;i<s.length();i++)
-            mp[s[i]]++;
-        //map is ready
-        
-        //map to vector of pairs
-        vector<pair<char,int>> v(mp.begin(),mp.end());
-        //vector is ready
-        
+        unordered_map<char,int>mpp;
+        for(int i=0;i<s.size();i++)
+        {
+            mpp[s[i]]++;
+        }
+        vector<pair<char,int>>v;
+        for(auto p:mpp)
+        {
+            v.push_back({p.first,p.second});
+        }
         sort(v.begin(),v.end(),comp);
         
-        string res="";
+        string ans="";
         for(auto it:v)
         {
             for(int i=0;i<it.second;i++)
-                res+=it.first;
+            {
+                ans+=it.first;
+            }
         }
+        return ans;
         
-        return res;
     }
 };
