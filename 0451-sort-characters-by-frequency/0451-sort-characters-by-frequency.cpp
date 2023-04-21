c9abbe17893ae7c949364d984d351803e2ad690c
Ishan Purohit <68988267+Ishan-77@users.noreply.github.com>
2023-04-21 23:43:55 +0530

    Time: 11 ms (74.80%), Space: 8.5 MB (45.80%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -13,14 +13,14 @@ public:
             mpp[s[i]]++;
         }
         vector<pair<char,int>>v;
-        for(auto p:mpp)
+        for(auto &p:mpp)
         {
             v.push_back({p.first,p.second});
         }
         sort(v.begin(),v.end(),comp);
         
         string ans="";
-        for(auto it:v)
+        for(auto &it:v)
         {
             for(int i=0;i<it.second;i++)
             {
