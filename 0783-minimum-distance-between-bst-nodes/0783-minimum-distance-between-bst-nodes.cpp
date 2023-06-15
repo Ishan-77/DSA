231d21fec5219708a954f10a21953353c44e9d9d
Ishan Purohit <68988267+Ishan-77@users.noreply.github.com>
2023-06-15 10:56:23 +0530

    Time: 3 ms (64.70%), Space: 9.7 MB (52.91%) - LeetHub

--- 0783-minimum-distance-between-bst-nodes/0783-minimum-distance-between-bst-nodes.cpp
+++ 0783-minimum-distance-between-bst-nodes/0783-minimum-distance-between-bst-nodes.cpp
@@ -11,22 +11,26 @@
  */
 class Solution {
 public:
-    
     int ans = INT_MAX;
-    int prev =  -1;
-    void solve(TreeNode*root) {
-       if(root == NULL) return;
+    int prev  = -1;
+    void helper(TreeNode* root)
+    {
+        if(root == NULL) return;
         
-        solve(root->left);
-        if(prev!=-1) {
-            ans = min(ans,root->val-prev);
+         helper(root->left);
+        
+        if(prev!=-1)
+        {
+            ans =  min(ans,abs(root->val - prev));
         }
-        prev = root->val;
+        prev =  root->val;
         
-        solve(root->right);
+         helper(root->right);
+        
     }
     int minDiffInBST(TreeNode* root) {
-        solve(root);
+        
+        helper(root);
         return ans;
     }
 };
