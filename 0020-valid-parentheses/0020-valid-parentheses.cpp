73320878ea74971a73004f607f32ea628e8912dc
Ishan Purohit <68988267+Ishan-77@users.noreply.github.com>
2023-06-20 23:54:17 +0530

    Time: 4 ms (40.32%), Space: 6.3 MB (40.16%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        
+        stack<char>st;
+        
+        for(auto i:s)
+        {
+            if(i=='('||i=='['||i=='{')
+            {
+                st.push(i);
+            }
+            else
+            {
+                if(st.empty() || (st.top()=='(' && i!=')')|| (st.top()=='{' && i!='}')|| (st.top()=='['&& i!=']'))
+                {
+                    return false;
+                }
+                st.pop();
+            }
+        }
+        if(st.empty())
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+        
+    }
+};
