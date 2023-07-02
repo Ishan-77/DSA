9859ed9cefbc1c34e06c79ed8df4af00f0de45a9
Ishan Purohit <68988267+Ishan-77@users.noreply.github.com>
2023-07-02 16:51:53 +0530

    Time: 3 ms (67.54%), Space: 8.2 MB (28.13%) - LeetHub

--- 0067-add-binary/0067-add-binary.cpp
+++ 0067-add-binary/0067-add-binary.cpp
@@ -1,26 +1,49 @@
 class Solution {
 public:
-    string addBinary(string a, string b) {
-        int alen=a.length();
-        int blen=b.length();
-        int i=0, carry=0;
-        string ans="";
+    string addBinary(string a, string b) 
+    {
+   reverse(a.begin(),a.end());
+    reverse(b.begin(),b.end());
+    int i=0,j=0;
+    int asize=a.length(), bsize=b.length();
+     int carry=0;
+     string ans="";
+     int x,y;
+    while(i<asize || j<bsize)
+    {   int sum;
+        x=0;
+        y=0;
+
+        if(i<asize)
+        x=a[i]-'0';
+        if(j<bsize)
+        y=b[j]-'0';
         
-        while (i<alen || i<blen || carry!=0){
-            int x=0;
-            if (i<alen && a[alen-i-1]=='1'){
-                x=1;
-            }
-            
-            int y=0;
-            if(i<blen and b[blen-i-1]=='1'){
-                y=1;
-            }
-            ans=to_string((x+y+carry)%2) + ans;
-            carry=(x+y+carry)/2;
-            i+=1;
+        sum=carry+x+y;
+        int digit;
+        if (sum<2)
+        {
+          digit=sum;
+          carry=0;
+        } 
+        else if (sum==2)
+        {
+            digit=0;
+            carry=1;
         }
-        return ans;
-            
+        else if(sum==3)
+        {
+           digit=1;
+           carry=1;
+        }
+       ans=to_string(digit)+ans;
+       i++;
+       j++;
+    }
+    if(carry==1)
+    ans=to_string(carry)+ans;
+    
+
+    return ans;
     }
 };
