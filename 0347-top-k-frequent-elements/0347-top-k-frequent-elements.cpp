fe5a36441199f8bcaaac7eed722ad31f5c0a80d0
Ishan Purohit <68988267+Ishan-77@users.noreply.github.com>
2023-07-13 11:51:09 +0530

    Time: 22 ms (27.96%), Space: 13.5 MB (96.34%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,27 +1,55 @@
-class Solution {
-public:
-    
-    bool static comp(pair<int,int>a,pair<int,int>b)
-    {
-        return a.second>b.second;
-    }
-    vector<int> topKFrequent(vector<int>& nums, int k) {
-        unordered_map<int,int>mpp;
-        vector<int> ans;
-        
-        for(int i=0;i<nums.size();i++)
-        {
-            mpp[nums[i]]++; // storing frequency
-        }
-        vector<pair<int,int>>v(mpp.begin(),mpp.end()); // map->vector of pair conversion 
-        
-        sort(v.begin(),v.end(),comp);
-        
-        for(int i=0;i<k;i++)
-        {
-            ans.push_back(v[i].first);
-        }
-        return ans;
-        
-    }
-};
+class Solution { 
+
+public: 
+
+    typedef pair<int, int> pd; 
+
+    struct myComp { 
+
+     bool operator()( pair<int, int> a,pair<int, int>  b) 
+
+    { 
+
+        return a.second < b.second; 
+
+    } 
+
+}; 
+
+    vector<int> topKFrequent(vector<int>& nums, int k)  
+
+    {   
+
+        vector<int> ans; 
+
+        unordered_map<int,int> mpp; 
+
+        for(int i=0;i<nums.size();i++) 
+
+        mpp[nums[i]]++; 
+
+        priority_queue<pd, vector<pd>, myComp> pq; 
+
+        for(auto it:mpp) 
+
+        pq.push(it);//max heap is ready 
+
+        for(int i=0;i<k;i++) 
+
+        { 
+
+            auto it=pq.top(); 
+
+            ans.push_back(it.first); 
+
+            pq.pop(); 
+
+        } 
+
+       
+
+      return ans; 
+
+   } 
+
+}; 
