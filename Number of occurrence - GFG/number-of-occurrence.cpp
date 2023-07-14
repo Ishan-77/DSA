a55706ac49b6656c758d260947d522c2e4e1ccec
Ishan Purohit <68988267+Ishan-77@users.noreply.github.com>
2023-07-14 08:23:25 +0530

    Added solution - LeetHub

---
+++ Number of occurrence - GFG/number-of-occurrence.cpp
@@ -0,0 +1,86 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+	/* if x is present in arr[] then returns the count
+		of occurrences of x, otherwise returns 0. */
+	int count(int arr[], int n, int x) {
+	    
+	    // code here
+	   int first =0;
+	   int last  = -1;
+	   
+	   int l = 0;
+	   int h = n-1;
+	   
+	   while(l<=h)
+	   {
+	       int mid = l+(h-l)/2;
+	       
+	       if(arr[mid] == x)
+	       {
+	           first  = mid;
+	           h =  mid-1;
+	           
+	       }
+	       else if(arr[mid] > x)
+	       {
+	           h = mid-1;
+	       }
+	       else
+	       {
+	           l = mid+1;
+	       }
+	   }
+	   l =0;
+	   h = n-1;
+	   
+	   while(l<=h)
+	   {
+	       int mid = l+(h-l)/2;
+	       
+	       if(arr[mid] == x)
+	       {
+	           last = mid;
+	           l = mid+1;
+	       }
+	       else if(arr[mid] > x)
+	       {
+	           h = mid-1;
+	       }
+	       else
+	       {
+	           l = mid+1;
+	       }
+	   }
+	   return last - first +1;
+	    
+	}
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, x;
+        cin >> n >> x;
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        auto ans = ob.count(arr, n, x);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
