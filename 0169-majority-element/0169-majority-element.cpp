86bd497c9b670ae52c730d78151ac66dad28c313
Ishan Purohit <68988267+Ishan-77@users.noreply.github.com>
2023-08-04 20:46:30 +0530

    Time: 11 ms (97.19%), Space: 19.5 MB (52.59%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,18 +1,23 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        unordered_map<int,int>mpp;
-        for(int i=0;i<nums.size();i++){
-            mpp[nums[i]]++;
-        }
-        int maxi = nums[0];
-        for(auto it:mpp) {
-            if(it.second > mpp[maxi]) {
-                maxi  =  it.first;
-            }
+        
+        int candidate = nums[0];
+        int life  =1;
+        
+        for(int i=1;i<nums.size();i++) {
+            if(nums[i]==candidate) life++;
             
+            else if(life==0) {
+                candidate = nums[i];
+                life = 1;
+                
+            }
+            else {
+                life--;
+            }
         }
-        return maxi;
+        return candidate;
         
     }
 };
