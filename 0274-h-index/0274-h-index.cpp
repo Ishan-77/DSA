5446c99867aac342abdba36d8c76a35dd28c537d
Ishan Purohit <68988267+Ishan-77@users.noreply.github.com>
2023-08-20 13:23:21 +0530

    Time: 5 ms (28.60%), Space: 8.7 MB (23.63%) - LeetHub

---
+++ 0274-h-index/0274-h-index.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+        
+        int l =0;
+        int h = citations.size();
+        
+        int ans =0;
+        
+        int count;
+        
+        while(l<=h) {
+            count=0;
+            int mid = l+(h-l)/2;
+            
+            for(int i=0;i<citations.size();i++) {
+                if(citations[i]>= mid)count++;
+            }
+            if(count>=mid) {
+                ans = mid;
+                l = mid+1;
+            }
+            else {
+                h = mid-1;
+            }
+        }
+        return ans;
+        
+    }
+};
