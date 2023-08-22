35090f3a8dafe5010c7d94e2504dfdca5ca2a825
Ishan Purohit <68988267+Ishan-77@users.noreply.github.com>
2023-08-22 17:03:48 +0530

    Time: 2 ms (49.78%), Space: 6.1 MB (11.31%) - LeetHub

---
+++ 0168-excel-sheet-column-title/0168-excel-sheet-column-title.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string convertToTitle(int n) {
+        
+        string res="";
+        
+        while(n) {
+            char c = 'A' + (n-1)%26;
+            res  = c+res;
+            
+            n = (n-1)/26;
+            
+        }
+        
+        return res;
+        
+    }
+};
