c6eb42b081337a2590e86508cccc31c0c755eb44
Ishan Purohit <68988267+Ishan-77@users.noreply.github.com>
2023-08-24 16:48:33 +0530

    Time: 2 ms (41.61%), Space: 6 MB (74.79%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,33 +1,19 @@
 class Solution {
 public:
-    
-    
-    int TotalWays(int currentstep, int targetstep ,unordered_map<int,int> &mpp)
-    {
-        if(currentstep == targetstep) return 1;
-
-        if(currentstep > targetstep) return 0;
-        
-        
-        int currentkey = currentstep;
-        
-        if(mpp.find(currentkey)!=mpp.end())
-        {
-            return mpp[currentkey];
-        }
-        int s1 = TotalWays(currentstep+1,targetstep,mpp);
-        int s2 =  TotalWays(currentstep+2,targetstep,mpp);
-        
-        mpp[currentkey] = s1+s2;
-        
-        return s1+s2;
-    }
-    
-    
-    
-    
     int climbStairs(int n) {
-        unordered_map<int,int>mpp;
-        return TotalWays(0,n,mpp);
+     
+        if(n<=2) return n;
+        
+        int prev = 2;
+        int prev2 = 1;
+        
+        for(int i=3;i<=n;i++) {
+            int curr = prev + prev2;
+             prev2 = prev;
+            prev = curr;
+            
+        }
+        return prev;
+        
     }
 };
