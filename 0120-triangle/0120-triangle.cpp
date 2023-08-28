2187579b550e1b74dc676ad50ea96cf3133e9ec8
Ishan Purohit <68988267+Ishan-77@users.noreply.github.com>
2023-08-28 09:27:14 +0530

    Time: 3 ms (92.63%), Space: 9.1 MB (21.93%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
-    
-    int func(int i,int j, vector<vector<int>>&triangle,vector<vector<int>>&dp) {
-        int n = triangle.size();
-        if(i==n-1) return triangle[n-1][j];
-        
-        if(dp[i][j] != -1) return dp[i][j];
-        
-        int down = triangle[i][j] + func(i+1,j,triangle,dp);
-        
-        int diag = triangle[i][j] +  func(i+1,j+1,triangle,dp);
-        
-        return dp[i][j] = min(down,diag);
-    }
     int minimumTotal(vector<vector<int>>& triangle) {
         int n = triangle.size();
+        vector<vector<int>>dp(n,vector<int>(n,-1)); // dp array
         
-        vector<vector<int>>dp(n,vector<int>(n,-1));
+        for(int j=0;j<n;j++) {
+            dp[n-1][j] = triangle[n-1][j]; // base cases 
+        }
         
-        return func(0,0,triangle,dp);
+        for(int i=n-2;i>=0;i--) {
+            for(int j=i;j>=0;j--) {
+                int down = dp[i+1][j] + triangle[i][j];
+                int diag = dp[i+1][j+1] + triangle[i][j];
+                
+                dp[i][j] =  min(down,diag);
+            }
+        }
+        return dp[0][0];
+        
     }
 };
