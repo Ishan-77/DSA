d7ba1d001fec8650e224672446c8fa6b68941e68
Ishan Purohit <68988267+Ishan-77@users.noreply.github.com>
2023-09-08 19:51:11 +0530

    Time: 8 ms (46.50%), Space: 15.2 MB (25.54%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,37 +1,37 @@
 class Solution {
 public:
     
-    int func(int idx,int buy,vector<int>&prices,vector<vector<int>>&dp) {
-        // base condition
-        
-        if(idx>=prices.size()) return 0;
-        
-        // explore all different possibilities 
-        
-        if(dp[idx][buy]!=-1) return dp[idx][buy];
-        
-        int profit;
-        
-        if(buy==1) {
-           int decide_buy = -prices[idx] + func(idx+1,0,prices,dp); // buy 
-            int decide_notbuy =  0+func(idx+1,1,prices,dp);// not buy 
-            profit = max(decide_buy,decide_notbuy);
-            
-        }
-        else {
-            int decide_sell  =prices[idx]  + func(idx+1,1,prices,dp);
-            int decide_notsell = 0 + func(idx+1,0,prices,dp);
-            profit = max(decide_sell,decide_notsell);
-        }
-        return  dp[idx][buy] = profit;
-    }
     int maxProfit(vector<int>& prices) {
         
         int n  = prices.size();
         
-        vector<vector<int>>dp(n,vector<int>(2,-1));
+        vector<vector<int>>dp(n+1,vector<int>(2,0));
         
-        return func(0,1,prices,dp);
+        dp[n][0] = 0;
+        dp[n][1] = 0;
+        
+         int profit;
+        
+        for(int idx=n-1;idx>=0;idx--) {
+            for(int buy=1;buy>=0;buy--) {
+                if(buy==1) {
+           int decide_buy = -prices[idx] + dp[idx+1][0]; // buy 
+            int decide_notbuy =  0+dp[idx+1][1];// not buy 
+            profit = max(decide_buy,decide_notbuy);
+            
+        }
+        else {
+            int decide_sell  =prices[idx]  + dp[idx+1][1];
+            int decide_notsell = 0 + dp[idx+1][0];
+            profit = max(decide_sell,decide_notsell);
+        }
+          dp[idx][buy] = profit;
+                
+            }
+        }
+        return dp[0][1];
+        
+        
         
     }
 };
