11bb380a42ab1edecbe45979b1afdf5265d64e36
Ishan Purohit <68988267+Ishan-77@users.noreply.github.com>
2023-10-05 16:42:37 +0530

    Time: 12 ms (30.64%), Space: 16.3 MB (18.73%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -2,44 +2,20 @@ class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
         
+        int n = nums.size();
+        
+        unordered_map<int,int>mpp;
         vector<int>ans;
-        int candidate1=0;
-        int candidate2=0;
-        int life1=0;
-        int life2=0;
         
         for(int i=0;i<nums.size();i++) {
-            if(nums[i]==candidate1) {
-                life1++;
-            }
-            else if(nums[i]==candidate2) {
-                life2++;
-            }
-            else if(life1==0) {
-                candidate1 =  nums[i];
-                life1=1;
-            }
-            else if(life2==0) {
-                candidate2 =  nums[i];
-                life2 = 1;
-            }
-            else {
-                life1--;
-                life2--;
-            }
-            
+            mpp[nums[i]]++;
         }
-        // we iterate again to get our ans
-        life1=0;
-        life2=0;
-        int tar =  floor(nums.size()/3);
-          
-        for(int i=0;i<nums.size();i++) {
-            if(nums[i]==candidate1) life1++;
-            else if(nums[i]==candidate2) life2++;
+        for(auto it: mpp) {
+            if(it.second >n/3 ) {
+                ans.push_back(it.first);
+            }
         }
-        if(life1 > tar) ans.push_back(candidate1);   // both if statement because both ans can be correct
-        if( life2 > tar) ans.push_back(candidate2);
         return ans;
+        
     }
 };
