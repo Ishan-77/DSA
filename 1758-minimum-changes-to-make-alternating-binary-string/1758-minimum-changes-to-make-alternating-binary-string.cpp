3cf7571f54261384af5a90a3b761b2714a83ef43
Ishan Purohit <68988267+Ishan-77@users.noreply.github.com>
2023-12-29 16:48:46 +0530

    Time: 5 ms (42.98%), Space: 7.3 MB (82.74%) - LeetHub

--- 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.cpp
+++ 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.cpp
@@ -2,31 +2,15 @@ class Solution {
 public:
     int minOperations(string s) {
         
-        int count_zero  = 0;
-        int count_one  = 0;
+        int count_zero = 0;
+        int n = s.size();
         
         
         for(int i=0;i<s.size();i++) {
-            if(i%2==0) {
-                if(s[i]=='0') {
-                    count_one++;
-                }
-                else {
-                    count_zero++;
-                }
-            }
-            else {
-                if(s[i]=='0') {
-                    count_zero++;
-                }
-                else {
-                    count_one++;
-                }
-            }
-            
-            
+            if(i%2==0 && s[i]=='1') count_zero++; 
+            if(i%2==1 && s[i]=='0') count_zero++;
         }
-        return min(count_zero,count_one);
+        return min(count_zero,n-count_zero);
         
     }
 };
