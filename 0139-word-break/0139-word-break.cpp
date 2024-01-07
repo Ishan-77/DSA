e6d14a6a269f56a55caabddaf235b154abbf9c35
Ishan Purohit <68988267+Ishan-77@users.noreply.github.com>
2024-01-07 23:45:09 +0530

    Time: 13 ms (43.39%), Space: 13.5 MB (41.26%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -1,25 +1,38 @@
 class Solution {
 public:
     bool wordBreak(string s, vector<string>& wordDict) {
-        unordered_set<string> st;
+        
+        unordered_set<string>st;
+        
         int n = s.size();
-
-        vector<int> dp(n + 1, false);
-        dp[0] = true; // Empty string is considered breakable
-
-        for (const string& word : wordDict) {
-            st.insert(word);
+        
+        
+        vector<int>dp(n+1,false);
+        
+        dp[0] = true;
+        
+        
+        for(int i=0;i<wordDict.size();i++) {
+            st.insert(wordDict[i]);
         }
-
-        for (int i = 1; i <= n; i++) {
-            for (int j = 0; j < i; j++) {
-                if (dp[j] && st.find(s.substr(j, i - j)) != st.end()) {
-                    dp[i] = true; // Update dp[i] if substring [j, i) is in the dictionary
-                    break;
+        for(int i=1;i<=n;i++) {
+            for(int j=0;j<i;j++) {
+                if(i==j) {
+                    if(st.find(s.substr(0,j+1))!=st.end()) {
+                        dp[i] = true;
+                        break;
+                    }
+                }
+                else if(dp[j]) {
+                    if(st.find(s.substr(j,i-j))!=st.end()) {
+                        dp[i] = true;
+                        break;
+                    }
+                    
                 }
             }
         }
-
-        return dp[n]; // Return the result for the entire string
+        return dp[n];
+        
     }
 };
