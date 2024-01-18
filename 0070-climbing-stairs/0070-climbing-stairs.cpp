1aa1dc784cf1089b54b84ee3e6416ff55e52fe55
Ishan Purohit <68988267+Ishan-77@users.noreply.github.com>
2024-01-18 09:08:24 +0530

    Time: 0 ms (100.00%), Space: 6.8 MB (8.66%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,19 +1,16 @@
 class Solution {
 public:
     int climbStairs(int n) {
-     
-        if(n<=2) return n;
         
-        int prev = 2;
-        int prev2 = 1;
+        vector<int>dp(n+1,0);
         
-        for(int i=3;i<=n;i++) {
-            int curr = prev + prev2;
-             prev2 = prev;
-            prev = curr;
-            
+        
+        dp[0] = 1;
+        dp[1] = 1;
+        
+        for(int i=2;i<=n;i++) {
+            dp[i] = dp[i-1] + dp[i-2];
         }
-        return prev;
-        
+        return dp[n];
     }
 };
