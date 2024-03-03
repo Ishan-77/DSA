d460445ecaae7fefa56b2a95b4c0f13c0ece8bc6
Ishan Purohit <68988267+Ishan-77@users.noreply.github.com>
2024-03-03 22:08:22 +0530

    Time: 3 ms (64.94%), Space: 13.3 MB (45.90%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
@@ -11,20 +11,22 @@
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-       ListNode* slow = head;
         ListNode* fast = head;
-        while(n--)
-        {
+        ListNode* slow = head;
+        
+        
+        while(n--) {
             fast = fast->next;
         }
         if(!fast) return slow->next;
         
-        while(fast->next)
-        {
-            slow = slow->next;
+        while(fast->next){
             fast = fast->next;
+            slow = slow->next;
         }
         slow->next = slow->next->next;
         return head;
+        
+        
     }
 };
